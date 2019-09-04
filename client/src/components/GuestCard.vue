35188a9f49752baaa9a1dde932514c7b5400cd8e
Gillian Shanks <GillianShanks88@gmail.com>
2019-09-04 15:41:57 +0100

    adds guest cards

--- client/src/components/GuestCard.vue
+++ client/src/components/GuestCard.vue
@@ -1,12 +1,30 @@
 <template lang="html">
-
+  <div class="listItem">
+    <h2>{{guest.name}}</h2>
+    <p>{{guest.email}}</p>
+    <div class="checkedIn">
+      <p>Checked in?</p>
+      <p v-if="guest.checkedIn">Yes!</p>
+      <p v-if="!guest.checkedIn">Not yet...</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "guest-card"
+  name: "guest-card",
+  props: ["guest"]
 }
 </script>
 
 <style lang="css" scoped>
+.listItem{
+  border: 2px black solid;
+  border-width: 4px;
+}
+
+.checkedIn{
+  display: flex;
+  flex-direction: row;
+}
 </style>
